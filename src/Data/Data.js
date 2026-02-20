e290661eb11f6d5b2bb0c0f87c3f4372ff233587
Suraj Gupta <120416519+surajgupta9161@users.noreply.github.com>
2026-02-20 17:10:02 +0530

    Update Data.js

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,6 +1,6 @@
 const Data = [
   {
-    title: 'MusicHub:',
+    title: 'MusicHub =>',
     description:
       'MusicHub is a MERN-based role-based music and video sharing platform where users and artists can securely upload, manage, and stream media content. It features a modern React + Tailwind UI, JWT authentication with HTTP-only cookies, protected routes, and media handling via ImageKit—built on a scalable Express and Node.js API.',
     technologies: [
@@ -15,7 +15,7 @@ const Data = [
   },
 
   {
-    title: 'SearchMedia:',
+    title: 'SearchMedia =>',
     description:
       'SearchMedia is a React-based media search web app that lets users find photos, videos, and GIFs on a single platform. It integrates Unsplash, Pexels, and Tenor APIs to deliver high-quality media, uses Redux Toolkit for state management, and LocalStorage to save favorites—providing a fast, smooth, and user-friendly search experience and also use React-Router-Dom.',
     technologies: ['React.Js', 'Redux-toolkit'],
@@ -23,7 +23,7 @@ const Data = [
     live: 'https://search-media-qdpl.vercel.app/'
   },
   {
-    title: 'WanderLust:',
+    title: 'WanderLust =>',
     description:
       'Built a full-stack room/hotel listing platform using Node.js, Express, MongoDB, EJS, and Bootstrap. Implemented secure authentication with Passport.js, REST APIs for listings and reviews, and a responsive UI.',
     technologies: ['Node.js', 'Express', 'MongoDB', 'Bootstrap', 'EJS'],
@@ -31,21 +31,21 @@ const Data = [
     live: 'https://wanderlust-8kue.onrender.com'
   },
   {
-    title: 'Personal Portfolio:',
+    title: 'Personal Portfolio =>',
     description:
       'Developed a modern and responsive portfolio website using HTML, CSS, JavaScript, and React. Showcased projects, technical skills, and contact information using reusable React components and clean UI design. Focused on performance, responsiveness, and maintainable code structure.',
     technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
     link: 'https://github.com/surajgupta9161/portfolio'
   },
   {
-    title: 'Quiz App:',
+    title: 'Quiz App =>',
     description:
       'A responsive Quiz App built with React that presents multiple-choice questions. Users get instant feedback on their answers, with correct options highlighted and wrong ones in red, while tracking their score and allowing quiz reset.',
     technologies: ['JavaScript', 'React', 'HTML/CSS'],
     link: 'https://github.com/surajgupta9161/QuizeApp'
   },
   {
-    title: 'Weather App:',
+    title: 'Weather App =>',
     description:
       'A responsive Weather App built with React and Material- UI that fetches real - time weather data from WeatherAPI.Users can search for any city, view temperature, humidity and wind details, and local time, with proper error handling for invalid inputs.',
     technologies: ['JavaScript', 'HTML', 'CSS', 'React'],
